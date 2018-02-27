58b175b6a3ec287340ef936769b94c2179157bfc
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-26 18:43:46 -0800

    more practice on migration

--- database/migrations/2018_02_27_023827_add_is_admin_column_to_posts_tables.php
+++ database/migrations/2018_02_27_023827_add_is_admin_column_to_posts_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddIsAdminColumnToPostsTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->text('content');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->tinyInteger('is_admin')->default('0');
+
         });
     }
 
@@ -28,6 +27,9 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->dropColumn('is_admin');
+        });
     }
 }
