b1a651f810ee5c3ae96be2f65b8d473da6404fb1
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-26 17:05:50 -0800

    practice model and controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::resource('posts', 'PostsController');
+
+
+
+//Route::get('/post/{id}', "PostsController@index");
+
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//
+//});
+//
+//Route::get('/about', function () {
+//
+//    return "Hi about page";
+//
+//});
+//
+//Route::get('/contact', function () {
+//    return "Hi I am contact";
+//
+//});
+//
+//Route::get('/post/{id}/{name}', function ($id, $name) {
+//
+//    return "This is post number ". $id . " " . $name;
+//
+//});
+//
+//
+//Route::get('admin/posts/example', array('as' => 'admin.home', function () {
+//
+//    $url = route('admin.home');
+//
+//    return "this url is ". $url;
+//
+//
+//}));
+
