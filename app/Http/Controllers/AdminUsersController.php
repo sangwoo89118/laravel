905b42f9525eb3b2af2e3b801390638a68b01162
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-28 04:09:43 -0800

    form validation for creating user and uploading files for admin page

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Role;
+use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
 {
@@ -29,7 +31,10 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+        $roles = Role::pluck('name', 'id')->all();
+
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -38,10 +43,17 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
-        
+
+
+        User::create($request->all());
+
+
+        return redirect('/admin/users');
+//        return $request->all();
+
     }
 
     /**
