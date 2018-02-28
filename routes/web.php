0799900614d18a1466167b8a5f1812144f640112
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-28 13:10:17 -0800

    routing admin posts

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', function(){
@@ -27,6 +29,8 @@ Route::group(['middleware'=>'admin'], function(){
 
 
     Route::resource('admin/users', 'AdminUsersController',['as'=>'admin']);
+
+    Route::resource('admin/posts', 'AdminPostsController',['as'=>'admin']);
 
 });
 
