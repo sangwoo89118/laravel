7151a0ec2ca471a6454572e9f504bdb05bab3c61
sangwoo89118 <sangwoo89118@gmail.com>
2018-02-28 15:31:26 -0800

    added images to edit post page

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -7,36 +7,55 @@
     <h1>Update Post</h1>
 
     <div class="row">
-        {!! Form::model($post,['method'=>'PATCH', 'action'=>['AdminPostsController@update', $post->id], 'files'=>true]) !!}
-        <div class="form-group">
-            {!! Form::label('title', 'Title:') !!}
-            {!! Form::text('title', null, ['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('category_id', 'Category:') !!}
-            {!! Form::select('category_id', [''=>'Choose Categories'] + $categories, null, ['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('photo_id', 'Photo:') !!}
-            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('body', 'Description:') !!}
-            {!! Form::textarea('body', null, ['class'=>'form-control', 'rows' => 5]) !!}
+
+
+        <div class="col-sm-6">
+
+
+            <img src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt="" class="img-responsive img-rounded">
         </div>
 
 
-        <div class="form-group">
-            {!! Form::submit('Create Post', ['class'=>'btn btn-primary pull-left']) !!}
-        </div>
-        {!! Form::close() !!}
 
 
-        {!! Form::open(['method'=>'DELETE', 'action'=>['AdminPostsController@destroy', $post->id] ]) !!}
-        <div class="form-group">
-            {!! Form::submit('Delete post', ['class'=>'btn btn-danger pull-right']) !!}
+        <div class="col-sm-6">
+
+            {!! Form::model($post,['method'=>'PATCH', 'action'=>['AdminPostsController@update', $post->id], 'files'=>true]) !!}
+            <div class="form-group">
+                {!! Form::label('title', 'Title:') !!}
+                {!! Form::text('title', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('category_id', 'Category:') !!}
+                {!! Form::select('category_id', [''=>'Choose Categories'] + $categories, null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo:') !!}
+                {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('body', 'Description:') !!}
+                {!! Form::textarea('body', null, ['class'=>'form-control', 'rows' => 5]) !!}
+            </div>
+
+
+            <div class="form-group">
+                {!! Form::submit('Create Post', ['class'=>'btn btn-primary pull-left']) !!}
+            </div>
+            {!! Form::close() !!}
+
+
+            {!! Form::open(['method'=>'DELETE', 'action'=>['AdminPostsController@destroy', $post->id] ]) !!}
+            <div class="form-group">
+                {!! Form::submit('Delete post', ['class'=>'btn btn-danger pull-right']) !!}
+            </div>
+            {!! Form::close() !!}
+
+
+
         </div>
-        {!! Form::close() !!}
+
+
     </div>
 
     <div class="row">
