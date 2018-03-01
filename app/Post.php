5c8f46da872341924f753a1c21e52d8981b9fb9d
sangwoo89118 <sangwoo89118@gmail.com>
2018-03-01 09:48:05 -0800

    modified post page dynamically

--- app/Post.php
+++ app/Post.php
@@ -32,5 +32,6 @@ class Post extends Model
 
 
 
+
 }
 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,9 @@
 
 @section('content')
 
+
+    <h1>Post</h1>
+
     <!-- Blog Post -->
 
     <!-- Title -->
@@ -12,27 +15,23 @@
 
     <!-- Author -->
     <p class="lead">
-        by <a href="#">Start Bootstrap</a>
+        by <a href="#">{{$post->user->name}}</a>
     </p>
 
     <hr>
 
     <!-- Date/Time -->
-    <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
+    <p><span class="glyphicon glyphicon-time"></span> Posted {{$post->created_at->diffForHumans()}}</p>
 
     <hr>
 
     <!-- Preview Image -->
-    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+    <img class="img-responsive" src="{{$post->photo->file}}" alt="">
 
     <hr>
 
     <!-- Post Content -->
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+    <p>{{$post->body}}</p>
 
     <hr>
 
