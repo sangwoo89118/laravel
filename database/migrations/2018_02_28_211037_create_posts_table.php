abd078c7faf5375fde5cb7cb28482bbb1c1216fe
sangwoo89118 <sangwoo89118@gmail.com>
2018-03-01 00:23:12 -0800

    setup for comments and reply

--- database/migrations/2018_02_28_211037_create_posts_table.php
+++ database/migrations/2018_02_28_211037_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
+            $table->integer('user_id');
             $table->integer('category_id')->unsigned()->index();
             $table->integer('photo_id')->unsigned()->index();
             $table->string('title');
